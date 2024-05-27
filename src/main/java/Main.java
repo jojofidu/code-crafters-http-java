f3392a75656492b85b0c3c99cb5b861ede5f508a
filipe.serina <filipe.serina@nokia.com>
2024-05-27 11:08:46 +0100

    3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,21 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-  private static int SERVER_PORT = 4221;
+
+  private static final int SERVER_PORT = 4221;
+  private static final String CRLF = "\r\n";
+  private static final String HTTP_VERSION = "HTTP/1.1";
+
+  private enum HttpStatus {
+    OK("200 OK"),
+    NOT_FOUND("404 Not Found");
+
+    private String key;
+    HttpStatus(String status) {
+      this.key = status;
+    }
+  }
+
   public static void main(String[] args) {
     System.out.println("Starting program");
 
@@ -16,9 +30,30 @@ public class Main {
       clientSocket = serverSocket.accept();
       System.out.println("accepted new connection");
 
-      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+      String request = new String(clientSocket.getInputStream().readNBytes(clientSocket.getInputStream().available()));
+      String response = handleRequestMappings(request);
+
+      clientSocket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  static String handleRequestMappings(String request) {
+    String[] requestLine = request.split(CRLF)[0].split(" ");
+    if (requestLine[0].equals("GET") && requestLine[1].equals("/")) {
+      return handleGetRoot();
+    } else {
+      return createHttpResponse(HttpStatus.NOT_FOUND);
+    }
+
+  }
+
+  static String handleGetRoot() {
+    return createHttpResponse(HttpStatus.OK);
+  }
+
+  static String createHttpResponse(HttpStatus httpStatus) {
+    return String.format("%s %s%s%s", HTTP_VERSION, httpStatus.key, CRLF, CRLF);
   }
 }
